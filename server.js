d19feed186c9c0b321195e62aca5d35fb8ba479e
madhur-taneja <tanejamadhur96@gmail.com>
2020-01-13 19:32:18 +0530

    Finishing Styling, Server and Logic

--- server.js
+++ server.js
@@ -2,18 +2,46 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`The app is running on ${port}`);
+});
+
+// GET Method
+app.get('/getRecentWeatherData', (req, res) => {
+    res.send(JSON.stringify(projectData));
+});
+
+// POST Method
+app.post('/saveData', (req, res) => {
+    console.log(req.body);
+    responseData = req.body;
+    let newWeatherData = {
+        temperature: responseData.temp,
+        date: responseData.date,
+        userResponse: responseData.userResponse
+    }
+    Object.assign(projectData, newWeatherData);
+    res.send(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,94 @@
 /* Global Variables */
+const base_URL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&APPID=bd13cd89e2841acdbab6011ea4942ce9'
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+/* Function called by event listener */
+function performAction() {
+	const zipCode = document.getElementById('zip').value;
+	const content = document.getElementById('feelings').value;
+
+	retrieveWeatherData(base_URL, zipCode, apiKey)
+		.then(function (data) {
+			console.log(data.main.temp + "TEmp");
+			postData('/saveData', {
+				temp: data.main.temp,
+				date: newDate,
+				userResponse: content
+			});
+		})
+		.then(function () {
+			getData("/getRecentWeatherData")
+				.then(function (data) {
+					console.log(data + 'data');
+					try {
+						document.getElementById('temp').innerHTML = data.temperature;
+						document.getElementById('date').innerHTML = data.date;
+						document.getElementById('content').innerHTML = data.userResponse;
+						console.log(data);
+					} catch (error) {
+						console.log("There is some error in getting the data -> " + error);
+					}
+				})
+		});
+};
+
+
+const retrieveWeatherData = async(base_URL, zipCode, apiKey) => {
+	const res = await fetch(base_URL + zipCode + apiKey);
+	try {
+		// Transform response into JSON
+		const data = await res.json()
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.log("Could not retrieve Weather Data", error);
+	}
+}
+
+//Function to POST the data
+const postData = async(url = '', data = {}) => {
+	console.log(data);
+	const response = await fetch(url, {
+		method: 'POST', // *GET, POST, PUT, DELETE, etc.
+		credentials: 'same-origin', // include, *same-origin, omit
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data), // body data type must match "Content-Type" header
+	});
+
+	try {
+		const newData = await response.json();
+		console.log(newData);
+		return newData;
+	} catch (error) {
+		console.log("error", error);
+		// appropriately handle the error
+	}
+}
+
+//Function to GET the data
+const getData = async(url = '') => {
+
+	const response = await fetch(url, {
+		method: 'GET',
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	try {
+		const newData = await response.json();
+		return newData;
+	} catch (error) {
+		console.log("GetData Error", error);
+	}
+}
